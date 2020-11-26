1fbd6825c9910f62a7fecdf24d88a1e0ec631262
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-11-26 00:15:57 -0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+import flask
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if (__name__ == '__main__'):
+    app.run(debug=False, host="66.11.118.34")
