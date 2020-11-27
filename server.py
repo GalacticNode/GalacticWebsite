37af3435e2904b5c9f2c398040b329cf66126dfa
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-11-27 16:19:01 -0700

    Update server.py

--- server.py
+++ server.py
@@ -8,4 +8,4 @@ def index():
     return render_template('index.html')
 
 if (__name__ == '__main__'):
-    app.run(debug=False, host="66.11.118.34")
+    app.run(debug=False, host="66.11.118.34", port="80")
